406e2ead2da3747849deb4b74ea0e5352cb4cfd1
WilSonS9 <54662723+WilSonS9@users.noreply.github.com>
2022-12-01 09:34:32 +0100

    Day 1

---
+++ 1.1.py
@@ -0,0 +1,7 @@
+with open('inp.txt', 'r') as f:
+    l = map(lambda f: f.split('\n'), f.read().split('\n\n'))
+
+l = list(map(lambda elf: sum(map(int, elf)), l))
+l.sort(reverse=True)
+
+print(l[0])
