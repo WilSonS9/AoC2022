e658ec5ade23a0bcfdbb49464aa153190ca5b024
WilSonS9 <54662723+WilSonS9@users.noreply.github.com>
2022-12-04 06:33:33 +0100

    Day 4

---
+++ 4.1.py
@@ -0,0 +1,12 @@
+with open('inp.txt', 'r') as f:
+    l = f.read().split('\n')
+
+c = 0
+
+for pair in l:
+    s1,s2 = pair.split(',')
+    l1,u1 = map(int, s1.split('-'))
+    l2,u2 = map(int, s2.split('-'))
+    c += (l1 <= l2 and u1 >= u2) or (l2 <= l1 and u2 >= u1)
+
+print(c)
